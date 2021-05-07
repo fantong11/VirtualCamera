3e887a9c9e69d391254a27ba9e5016c84e7b3edd
fantong <fankaihsiang11@gmail.com>
2021-05-07 21:02:47 +0800

    Add zoom Slider

---
+++ Assets/Script/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject zoomSliderPrefab;
+    public Transform canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject zoomSlider = Instantiate(zoomSliderPrefab);
+        zoomSlider.transform.SetParent(canvas, false);
+
+        //RectTransform zoomSliderTransform = zoomSlider.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

5cca1ea8f64ec25c28c6a612d72ef2151daef352
cmalvin121 <cmalvin121@gmail.com>
2021-10-06 23:07:09 +0800

    switchMap & green screen

---
+++ Assets/Script/SwitchMap.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchMap : MonoBehaviour
+{
+
+    public List<GameObject> MapList;
+    //[SerializeField]
+    //private GameObject Map;
+    public GameObject currentMap = null;
+    private int ptr = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickBtn2() {
+        if (currentMap != null) Destroy(currentMap);
+        ptr += 1;
+        if (ptr == MapList.Count) ptr = 0;
+        currentMap = Instantiate(MapList[ptr]);
+    }
+
+    public void OnClickBtn3() {
+        if (currentMap != null) Destroy(currentMap);
+        ptr -= 1;
+        if (ptr == -1) ptr = MapList.Count - 1;
+        currentMap = Instantiate(MapList[ptr]);
+    }
+}

e4050fe5173389e8a3d61c4f23602c8263d11323
Fantong <fankaihsiang11@gmaill.com>
2021-10-17 12:30:06 +0800

    add green background scene

--- Assets/Script/MenuManager.cs
+++ Assets/Script/MenuManager.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
     public Transform unityChan;
     public Transform kumasen;
 
+    [SerializeField]
+    private Transform toggleGreenBg;
+    private Toggle toggle;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        toggle = toggleGreenBg.GetComponent<Toggle>();
     }
 
     // Update is called once per frame
@@ -23,13 +28,23 @@ public class MenuManager : MonoBehaviour
 
     public void getStartKumasen()
     {
-        SceneManager.LoadScene("VirtualCamera");
         StaticClass.avatar = kumasen;
+        if (toggle.isOn)
+        {
+            SceneManager.LoadScene("VirtualCameraWithGreenBackground");
+            return;
+        }
+        SceneManager.LoadScene("VirtualCamera");
     }
 
     public void getStartUnityChan()
     {
-        SceneManager.LoadScene("VirtualCamera");
         StaticClass.avatar = unityChan;
+        if (toggle.isOn)
+        {
+            SceneManager.LoadScene("VirtualCameraWithGreenBackground");
+            return;
+        }
+        SceneManager.LoadScene("VirtualCamera");
     }
 }
